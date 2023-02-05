bc2795d6163a041fc958d1e111e5b02a8ce79246
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-06 01:06:31 +0500

    Fixes

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -17,17 +17,17 @@ const Hero = () => {
 
   return (
     <section
-      className="relative z-0 h-[50vh] md:h-[150vh] bg-no-repeat bg-center bg-cover bg-secondary"
+      className="relative z-0 h-screen md:h-[150vh] bg-no-repeat bg-center bg-cover bg-secondary"
       style={{ backgroundImage: `url(${HERO_PRIMARY_IMG})` }}
     >
-      <div className="absolute top-[57%] right-10 md:top-[60%]  lg:right-0 lg:top-[17.5%]">
+      <div className="absolute top-[70%] right-10 md:top-[60%] lg:right-0 lg:top-[17.5%]">
         <img
           src={HERO_SECONDARY_IMG}
           alt="Loading..."
           className="w-64 ml-24 md:w-full md:ml-0"
         />
       </div>
-      <div className="content h-[50vh] md:h-[130vh]  flex items-center">
+      <div className="content h-screen md:h-[130vh]  flex items-center">
         <div className="flex flex-col gap-3 mx-10 md:mx-20">
           <Text9Xl text="BEST GAME" classes="metal text-shadow mt-32 z-[1]" />
           <Text9Xl text="PLAYING TODAY." classes="metal text-shadow z-[1]" />
