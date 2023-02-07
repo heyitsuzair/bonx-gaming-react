836dc1692e824550e69a44c3398d801d29c50a80
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-08 03:01:18 +0500

    Click Disabled For Production

--- src/DisableClick.jsx
+++ src/DisableClick.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 const DisableClick = () => {
   useEffect(() => {
     document.addEventListener("contextmenu", (e) => {
-      process.env.VITE_IN_PRODUCTION && e.preventDefault();
+      import.meta.env.VITE_IN_PRODUCTION && e.preventDefault();
     });
   }, []);
 };
