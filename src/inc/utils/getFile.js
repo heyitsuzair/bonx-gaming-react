da86f0dd0523f42f08d97d0cf902595662f4848b
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-15 04:42:48 +0500

    Fixes

--- src/inc/utils/getFile.js
+++ src/inc/utils/getFile.js
@@ -1,4 +1,5 @@
 export const getFile = (name) => {
-  const HOST = import.meta.env.UPLOADS_URL || "http://localhost:5000/uploads/";
+  const HOST =
+    import.meta.env.VITE_UPLOADS_URL || "http://localhost:5000/uploads/";
   return HOST + name;
 };
